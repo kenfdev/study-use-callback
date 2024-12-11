0d3b237dfbe4f7557c7e382d9bfe56d364171600
Ken Fukuyama <kenfdev@gmail.com>
2024-12-11 09:10:01 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,23 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useCallback, useState } from 'react';
 import './App.css';
 import { CounterSelect } from './CounterSelect';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const handleCount = (value: number) => {
+  const handleCount = useCallback((value: number) => {
     setCount((prevCount) => prevCount + value);
-  };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <div>
           <CounterSelect onChange={handleCount} />
         </div>
         <h2>Selection count is: {count}</h2>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
